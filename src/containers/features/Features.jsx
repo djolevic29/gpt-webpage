730acb7793872ab163424883b7fc58262cde1c3d
Alex Djordjevic <alexandros.djordjevic@gmail.com>
2022-12-21 16:24:02 +0200

    Enter

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import Feature from '../../components/feature/Feature';
 import './features.css';
 
+
 const Features = () => {
-    return (
-        <div>
-            Features
-        </div>
-    )
+  return (
+    <div>
+      Features
+    </div>
+  )
 }
 
 export default Features
